d120511c806d104376249a7ed8932d1e6313815e
Nora VanDeVoorde <noravandevoorde@mac.attlocal.net>
2026-06-19 14:28:59 -0500

    done sampling

--- GetSamples.py
+++ GetSamples.py
@@ -14,20 +14,37 @@ for row in sheet.iter_rows(min_row=2, values_only=True):
         label_dict[label] = []
     label_dict[label].append(row)
 
-#grab a sample from each label
-sample_frac = 0.1  # 10% sample
-sample_lib = {}
+#map labels to numbers
+label_numbers = {}
+
+for label in label_dict.keys():
+    if label and label[0].isdigit():  # checks if the first character is a number
+        label_numbers[label] = int(label[0])
+
+#create a sample dictionary
+sample_dict = {}
 
 for label, rows in label_dict.items():
-    sample_size = int(len(rows) * sample_frac)
+    if label in label_numbers:
+        number = label_numbers[label]
+        
+        if number not in sample_dict:
+            sample_dict[number] = []
 
-    sample_lib[label] = random.sample(rows, sample_size)
+        sample_dict[number].extend(rows)
+
+#grab a sample from each label
+sample_frac = 0.1  # 10% sample
+
+for label, rows in sample_dict.items():
+    sample_size = int(len(rows) * sample_frac)
+    sample_dict[label] = random.sample(rows, sample_size)
 
 #store samples in sheets for validation
 output = Workbook()
 output.remove(output.active) 
 
-for label, rows in sample_lib.items():
+for label, rows in sample_dict.items():
     ws = output.create_sheet(f"Label_{label}")
     for row in rows:
         ws.append(row)
